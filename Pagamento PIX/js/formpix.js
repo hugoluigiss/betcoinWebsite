cedfb770cf90eb3a21e20b51211ceccef1d32033
Hugo Luigi <hugo-luigi@hotmail.com>
2022-08-08 12:21:35 -0400

    limitação preco

--- Pagamento PIX/js/formpix.js
+++ Pagamento PIX/js/formpix.js
@@ -8,6 +8,7 @@ const btnConfirmar = document.querySelector('.avancarFormPix');
 let qtdBetcoin = document.querySelector('#quantidadeBET');
 let qtdReal = document.querySelector('#realInput');
 let cotacaoEmRealBET = 0.44;
+const erroValorMinimo = document.getElementById('erroValorMinimo');
 
 const modal = document.querySelector('#modalPix');
 const botaoFecharModal = document.querySelector('#fecharModalPix');
@@ -15,6 +16,12 @@ const botaoFecharModal = document.querySelector('#fecharModalPix');
 
 
 btnCalcular.addEventListener('click', () => {
+    if(qtdReal.value < 70){
+        erroValorMinimo.innerHTML = "O valor mínimo é R$70,00."
+        return;
+    } else{
+        erroValorMinimo.innerHTML = ""
+    }
     let valorCalculado = Math.round(parseInt(qtdReal.value) / cotacaoEmRealBET);
     qtdBetcoin.textContent = `R$${qtdReal.value} = ${valorCalculado}BET`
 });
@@ -22,6 +29,12 @@ btnCalcular.addEventListener('click', () => {
 let form = document.getElementById('sheetdb-form');
 form.addEventListener("submit", e => {
     e.preventDefault();
+    if(qtdReal.value < 70){
+        erroValorMinimo.innerHTML = "O valor mínimo é R$70,00."
+        return;
+    }else{
+        erroValorMinimo.innerHTML = ""
+    }
     fetch(form.action, {
         method: "POST",
         body: new FormData(document.getElementById("sheetdb-form")),
